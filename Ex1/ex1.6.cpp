8a982664ad00fa850b05fe47db1fd51d552f666a
bizzpy <131017111+bizzpy@users.noreply.github.com>
2024-06-02 19:43:22 +0000

    fazendo ex6

--- Ex1/ex1.6.cpp
+++ Ex1/ex1.6.cpp
@@ -8,20 +8,63 @@ class Funcionario{
     Funcionario(){};
     ~Funcionario(){};
 
+    void setSal(float _sal){
+        sal = _sal;
+    }
+
+    void setMatricula(float _mat){
+        matricula = _mat;
+    }
+
+    void setNome(string _nome){
+        nome = _nome;
+    }
+
+    float getAno(){
+        ano = sal*12;
+    }
+
+    void exibir(){
+        std::cout << "Nome: " << nome << std::endl;
+        std::cout << "Salario: " << sal << std::endl;
+        std::cout << "Salario anual: " << ano << std::endl;
+    }
+
     private:
+    string nome;
     float sal;
-    float ga;
+    float ano;
+    int matricula;
 };
 
 int main(){
+
+    Funcionario user;
+
     string nome;
     float sal;
-    int c;
-    float ga;
+    string c;
+    float ano;
+    int matricula;
+
+    std::cout << "Digite t para tecnico ou a para administrativo: " << std::endl;
+    std::cin >> c;
+
+    if (c == "t"){
+
+    }
 
     std::cout << "Digite seu nome " << std::endl;
     std::cin >> nome;
+    user.setNome(nome);
 
     std::cout << "Digite seu salario " << std::endl;
     std::cin >> sal;
+    user.setSal(sal);
+
+    std::cout << "Digite sua matricula " << std::endl;
+    std::cin >> matricula;
+    user.setMatricula(matricula);
+
+    user.exibir();
 }
